30b958fc0a59cd887e50e3e23e60f2e5775ddf90
kkeerthana28 <89631321+kkeerthana28@users.noreply.github.com>
2021-09-03 15:28:31 +0530

    Create program 5.java

---
+++ program 5.java
@@ -0,0 +1,12 @@
+public class Overloading1{  
+	  public static void main(int a){  
+	  System.out.println(a); 
+}  
+
+    public static void main(String args[]) {
+      System.out.println("main() method invoked");  
+	  main(10);  
+
+    }
+}
+© 2021 GitHub, Inc.
