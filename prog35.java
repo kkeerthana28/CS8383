defab47fe7d7c388512fcae68500a800c6233e7c
kkeerthana28 <89631321+kkeerthana28@users.noreply.github.com>
2021-09-17 15:14:19 +0530

    Create prog35.java

---
+++ prog35.java
@@ -0,0 +1,9 @@
+public class TestSubstring{  
+	 public static void main(String args[]){  
+	String s="  Sachin  ";  
+	System.out.println(s);//  Sachin    
+	System.out.println(s.trim());//Sachin  
+
+}
+}
+© 2021 GitHub, Inc.
