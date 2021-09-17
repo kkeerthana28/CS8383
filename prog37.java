6559ec6bdb517d99cbdd0f18c4acb2549696f95d
kkeerthana28 <89631321+kkeerthana28@users.noreply.github.com>
2021-09-17 15:15:33 +0530

    Create prog37.java

---
+++ prog37.java
@@ -0,0 +1,11 @@
+public class TestSubstring{  
+	 public static void main(String args[]){  
+	String s="Sachin";  
+	System.out.println(s.charAt(0));//S  
+	System.out.println(s.charAt(3));//h
+
+  
+
+}
+}
+© 2021 GitHub, Inc.
