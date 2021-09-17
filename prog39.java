7086472d27ef44da15757a322407929df9b6e3d2
kkeerthana28 <89631321+kkeerthana28@users.noreply.github.com>
2021-09-17 15:18:06 +0530

    Create prog39.java

---
+++ prog39.java
@@ -0,0 +1,12 @@
+public class TestSubstring{  
+public static void main(String args[]){  
+String s=new String("Sachin");  
+String s2=s.intern();  
+System.out.println(s2);
+
+
+  
+
+}
+}
+© 2021 GitHub, Inc.
