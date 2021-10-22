d120b48fe6286236842f0fb242a8eb37efe8665b
kkeerthana28 <89631321+kkeerthana28@users.noreply.github.com>
2021-10-22 14:57:27 +0530

    Create 60 prg.java

---
+++ 60 prg.java
@@ -0,0 +1,8 @@
+public 	class A{  
+	public static void main(String args[]){  
+	StringBuilder sb=new StringBuilder("Hello");  
+	sb.reverse();  
+	System.out.println(sb);//prints olleH  
+	}  
+	} 
+
